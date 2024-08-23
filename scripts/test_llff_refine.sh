fa314e8c855ea072a49b6b083f38d1bc5c343238
DrawingProcess <sjchoi.dp@gmail.com>
2024-08-23 13:23:11 +0900

    Add: execute test code

--- scripts/test_llff_refine.sh
+++ scripts/test_llff_refine.sh
@@ -4,7 +4,7 @@ H=378
 # batch_size=32
 downscale=2
 batch_size=1
-option="patchsamearea8"
+option="patchsamearea1_codebook"
 
 data_root='/data/csj000714/data'
 
@@ -12,7 +12,8 @@ python test_refine.py --name llff-refine-${dataset}-${H}x${W}-ni-dp-ds${downscal
     --dataset_mode llff_refine --dataset_root ${data_root}/nerf_llff_data/${dataset} \
     --checkpoints_dir ./checkpoints/nerf-sr-refine/ --summary_dir ./logs/nerf-sr-refine --results_dir ./results/nerf-sr-refine \
     --img_wh $W $H --batch_size $batch_size \
-    --model refine --test_split test_train \
-    --syn_dataroot ./checkpoints/nerf-sr/llff-${dataset}-${H}x${W}-ni64-dp-ds${downscale}/30_val_vis 
+    --model refine --test_split test_train --load_epoch 3\
+    --syn_dataroot ./checkpoints/nerf-sr/llff-${dataset}-${H}x${W}-ni64-dp-ds${downscale}/30_test_vis \
+    --network_codebook --inference
 
     # --test_split test --refine_network maxpoolingmodel --load_epoch 3 \
